bda30566a5459638a01a3e2c28443c5fdd6c3f74
Joseph Brendan <jbrendan86@gmail.com>
2023-12-12 20:30:14 +0100

    recent

---
+++ index.js
@@ -0,0 +1,47 @@
+// Single case
+// Double case
+// Multiple case
+
+
+// let failLimit = 40
+// let passLimit = 50
+// let goodLimit = 60
+// let veryGoodLimit = 80
+// let excellentLimit = 100
+
+// let studentScore = window.prompt("Enter Your Exam Score")
+// studentScore = Number(studentScore)
+
+// if(studentScore <= failLimit){
+//     console.log("You have failed this course!")
+// }else if(studentScore > failLimit && studentScore <= passLimit){
+//     console.log("You earned a pass mark")
+// }else if(studentScore > passLimit && studentScore <= goodLimit){
+//     console.log("You earned a good grade")
+// }else if( studentScore > goodLimit && studentScore <= veryGoodLimit){
+//     console.log("You  earned a very good grade")
+// }else if(studentScore > veryGoodLimit && studentScore <= excellentLimit){
+//     console.log("You earned an excellent score")
+// }else{
+//     console.log("Seems you did not sit for the exam")
+// }
+
+// let myName = `Joseph`
+// let myStory = `My name is ${myName} and I am here`
+// console.log(myStory)
+
+
+let cash = 200
+let debit = 200
+
+// if(cash === debit){
+//     console.log("Warning! Your spending is too much")
+// }else{
+//     console.log("You can go ahead to pay or not")
+// }
+
+// Ternary Operators
+let outcome = cash === debit ? console.log("Warning! Your spending is too much") : console.log("You can go ahead to pay or not")
+
+
+joseph === brendan ? "Do this if true" : "Do this if not true"
---
+++ new.js
@@ -0,0 +1,61 @@
+// Exponent Operation
+// let exponentResult = 10 ** 4
+// console.log(exponentResult)
+
+// Increment Operation
+// let cartNumber = 1
+// cartNumber++
+// console.log(cartNumber)
+
+// Decrement Operation
+// let highCartNumber = 10
+// highCartNumber--
+// console.log(highCartNumber)
+
+// Addition Assignment Operation
+// let newNumber = 10
+// newNumber += 4
+// console.log(newNumber)
+
+// Subtraction Assignment Operation
+// let subtractedVariable = 20
+// subtractedVariable -= 10
+// console.log(subtractedVariable)
+
+// Multiplication Assignment Operation
+// let paymentMade = 3
+// paymentMade *= 100
+// console.log(paymentMade)
+
+// Division Assignment Operation
+// let dividedVariable = 730
+// dividedVariable /= 4
+// console.log(dividedVariable)
+
+
+// COMPARISON OPERATIONS IN JAVASCRIPT
+// let partyAge = 21
+// let andrewsAge = 20
+// let partyAgeChecker = andrewsAge == partyAge
+// console.log(partyAgeChecker)
+
+// let agreedCity = "New York"
+// let joansCity = "New York"
+// let cityChecker = joansCity == agreedCity
+// console.log(cityChecker)
+
+// let newPartyAge = 21
+// let johnsage = "21"
+// let newPartyAgeChecker = johnsage === newPartyAge
+// console.log(newPartyAgeChecker)
+
+
+// IF ELSE STATEMENTS
+
+
+// This function checks for age and city
+function NameOfFunction(){
+    console.log("I have tried")
+}
+
+NameOfFunction()
